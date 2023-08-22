0c8c71297c0cdd21b4a8f6ef29faa24075ed909a
Leonardo Theodoro <leonardo@recruitrobin.com>
2023-08-22 13:53:53 -0300

    :construction: value objects

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,8 +1,10 @@
 import { randomUUID } from "node:crypto"
+import { Slug } from "./value-objects/slug"
 
 interface QuestionProps {
   title: string,
   content: string,
+  slug: Slug,
   authorId: string,
 }
 
@@ -10,12 +12,15 @@ export class Question {
   public id: string
   public title: string
   public content: string
+  public slug: Slug
   public authorId: string
 
-  constructor({ authorId, content, title }: QuestionProps, id?: string) {
+  constructor({ authorId, content, title, slug }: QuestionProps, id?: string) {
     this.title = title
     this.content = content
+    this.slug = slug
     this.authorId = authorId
     this.id = id ?? randomUUID()
   }
+
 }
