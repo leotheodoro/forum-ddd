0e27c13f33cb5f66c7fb0d348d0a3f00f3657b2c
Leonardo Theodoro <leonardo@recruitrobin.com>
2023-08-22 21:20:05 -0300

    :construction_building: abstract entity creation

--- src/domain/entities/instructor.ts
+++ src/domain/entities/instructor.ts
@@ -1,8 +1,14 @@
-import { randomUUID } from "node:crypto"
 import { Entity } from "../../core/entities/entity"
+import { UniqueEntityID } from "../../core/entities/unique-entity-id"
 
 interface InstructorProps {
   name: string
 }
 
-export class Instructor extends Entity<InstructorProps> {}
+export class Instructor extends Entity<InstructorProps> {
+  static create(props: InstructorProps, id?: UniqueEntityID) {
+    const question = new Instructor(props, id)
+
+    return question
+  }
+}
