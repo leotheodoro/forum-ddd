cbb92495daa9c856e40f6acfa7c31ec15135fea2
Leonardo Theodoro <leonardo@recruitrobin.com>
2023-08-22 21:07:58 -0300

    :building_construction: unique entity id

--- src/core/entities/entity.ts
+++ src/core/entities/entity.ts
@@ -1,15 +1,16 @@
 import { randomUUID } from "node:crypto"
+import { UniqueEntityID } from "./unique-entity-id"
 
 export class Entity<Props> {
-  private _id: string
+  private _id: UniqueEntityID
   protected props: Props
 
   get id() {
-    return this._id
+    return this._id.toString()
   }
 
   constructor(props: Props, id?: string) {
-    this._id = id ?? randomUUID()
+    this._id = new UniqueEntityID(id)
     this.props = props
   }
 }
